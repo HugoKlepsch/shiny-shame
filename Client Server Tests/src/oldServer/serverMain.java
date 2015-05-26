687d2fbcc3858d073f35088a9ed966925a77db98
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-26 19:53:44 -0400

    Added Message class to hold a message to be sent over the network.

--- Client Server Tests/src/oldServer/serverMain.java
+++ Client Server Tests/src/oldServer/serverMain.java
@@ -10,11 +10,9 @@ package oldServer;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.Vector;
 
