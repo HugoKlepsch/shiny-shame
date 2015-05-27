e0f0deb5e94f784d1139b544c237c160930d98b5
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-26 21:10:39 -0400

    Added skels.

---
+++ Client Server Tests/src/client/ClientMain.java
@@ -0,0 +1,29 @@
+/*
+		 Title: ClientMain.java
+		 Programmer: hugo
+		 Date of creation: May 26, 2015
+		 Description: 
+*/
+
+
+package client;
+
+/**
+ * @author hugo
+ *
+ */
+public class ClientMain {
+
+	/**
+		 * @author hugo
+		 * Date of creation: May 26, 2015 
+		 * @param: None
+		 * @return: None
+		 * @Description: ( ͡° ͜ʖ ͡°)
+		 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Client Server Tests/src/client/ServerComms.java
@@ -0,0 +1,17 @@
+/*
+		 Title: ServerComms.java
+		 Programmer: hugo
+		 Date of creation: May 26, 2015
+		 Description: 
+*/
+
+
+package client;
+
+/**
+ * @author hugo
+ *
+ */
+public class ServerComms {
+
+}
