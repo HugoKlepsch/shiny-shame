65699067ca819fe33c0a70145e5a5e7dc0554606
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-27 13:19:47 -0400

    Client in comms comes into first working version

--- Client Server Tests/src/client/ServerInComms.java
+++ Client Server Tests/src/client/ServerInComms.java
@@ -8,16 +8,43 @@
 
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+
+import sharedPackages.ActionRequest;
+import sharedPackages.ActionTypes;
 
 /**
  * @author hugo
  *
  */
-public class ServerInComms {
-	
+public class ServerInComms extends Thread{
+	Socket scSocket;
+	ObjectInputStream scStream;
 	
 	public ServerInComms(Socket socket){
-		
+		scSocket = socket;
+	}
+	
+	public void run(){
+		try {
+			ActionRequest actionRequest;
+			scStream = new ObjectInputStream(scSocket.getInputStream());
+			while(ClientMain.StayAlive()){
+				actionRequest = (ActionRequest) scStream.readObject();
+				if(actionRequest.getAction() == ActionTypes.SENDCURRENTMESSAGEINDEX){
+					ClientMain.setRemoteIndex(actionRequest.getIndex());
+				} else if (actionRequest.getAction() == ActionTypes.RECIEVEMESSAGE) {
+					ClientMain.addMessage(actionRequest.getMessage());
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
