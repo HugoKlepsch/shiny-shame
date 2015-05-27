e1e578b79d4e721c8844bd242f3917a7af3b1b9c
Graham <ghildebrandt@outlook.com>
2015-05-26 21:36:04 -0400

    basics of servercomms

--- Client Server Tests/src/client/ServerComms.java
+++ Client Server Tests/src/client/ServerComms.java
@@ -1,6 +1,6 @@
 /*
 		 Title: ServerComms.java
-		 Programmer: hugo
+		 Programmer: graham
 		 Date of creation: May 26, 2015
 		 Description: 
 */
@@ -8,10 +8,42 @@
 
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import sharedPackages.LoginDeets;
+
 /**
- * @author hugo
+ * @author graham
  *
  */
 public class ServerComms {
+	private static int port = 6969;
+	private Socket socket;
+	private static ObjectInputStream scStream;
+	private static ObjectOutputStream csStream;
+	private LoginDeets userDeets;
+	private static final int GETCURRENTMESSAGEINDEX = 1;
+	private static final int GETMESSAGE = 2;
+	private static final int SENDMESSAGE = 3;
+	private static final int DISCONNECT = 4;
+	private static final int CONNECT = 5;
+	private String ipAddress;
+	private int localIndex;
+	
+	public ServerComms(String ipAddress, LoginDeets userDeets){
+		this.ipAddress = ipAddress;
+		this.userDeets = userDeets;
+	}
+	
+	public void run() throws UnknownHostException, IOException{
+		socket = new Socket(this.ipAddress, port);
+		scStream = new ObjectInputStream(socket.getInputStream());
+		csStream = new ObjectOutputStream(socket.getOutputStream());
+		
+	}
 
 }
--- Client Server Tests/src/server/Connection.java
+++ Client Server Tests/src/server/Connection.java
@@ -11,7 +11,6 @@ package server;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 
 import sharedPackages.ActionRequest;
