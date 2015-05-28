63841e0156b84d78a35e8989e7394300d3d9068f
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-27 20:38:36 -0400

    made wrong binaries, recompiled the correct ones.

--- Client Server Tests/src/server/Connection.java
+++ Client Server Tests/src/server/Connection.java
@@ -3,8 +3,7 @@
 		 Programmer: graham
 		 Date of creation: May 26, 2015
 		 Description: 
-*/
-
+ */
 
 package server;
 
@@ -28,19 +27,18 @@ public class Connection extends Thread {
 	private ObjectInputStream csStream;
 	private LoginDeets userDeets;
 	private static LoginDeets rootDeets = new LoginDeets("root", "dank");
-	
+
 	public Connection(Socket sock) {
 		this.outSocket = sock;
 	}
-	
-	
-	private void sendMsg(Message message) throws IOException{
+
+	private void sendMsg(Message message) throws IOException {
 		ActionRequest sendMsgRequest = new ActionRequest(ActionTypes.SCSENDMESSAGE, message);
 		scStream.writeObject(sendMsgRequest);
 		scStream.flush();
 	}
-	
-	public void run(){
+
+	public void run() {
 		try {
 			scStream = new ObjectOutputStream(outSocket.getOutputStream());
 			csStream = new ObjectInputStream(outSocket.getInputStream());
@@ -50,45 +48,44 @@ public class Connection extends Thread {
 			int currIndex;
 			do {
 				actionRequest = (ActionRequest) csStream.readObject();
-				if(actionRequest.getAction() == ActionTypes.CSGETCURRENTMESSAGEINDEX){
+				if (actionRequest.getAction() == ActionTypes.CSGETCURRENTMESSAGEINDEX) {
 					currIndex = mainThread.getCurrentMessageIndex();
 					ActionRequest sendIndexRequest = new ActionRequest(ActionTypes.SCSENDCURRENTMESSAGEINDEX, currIndex);
 					scStream.writeObject(sendIndexRequest);
 					scStream.flush();
-					
-				} else if(actionRequest.getAction() == ActionTypes.CSGETMESSAGE){
+
+				} else if (actionRequest.getAction() == ActionTypes.CSGETMESSAGE) {
 					System.out.println(userDeets.getUserName() + " " + actionRequest.getAction());
 					wantedIndex = actionRequest.getIndex();
 					message = mainThread.getMessage(wantedIndex);
 					sendMsg(message);
-				} else if(actionRequest.getAction() == ActionTypes.CSSENDMESSAGE){
+				} else if (actionRequest.getAction() == ActionTypes.CSSENDMESSAGE) {
 					System.out.println(userDeets.getUserName() + " " + actionRequest.getAction());
 					message = actionRequest.getMessage();
 					mainThread.addMessage(message);
-				} else if(actionRequest.getAction() == ActionTypes.CSCONNECT){
+				} else if (actionRequest.getAction() == ActionTypes.CSCONNECT) {
+					userDeets = actionRequest.getMessage().getCredentials();
 					System.out.println(userDeets.getUserName() + " " + actionRequest.getAction());
-					 userDeets = actionRequest.getMessage().getCredentials();
-					 String msg = userDeets.getUserName() + " connected";
-					 Message connectMsg = new Message(rootDeets, msg);
-					 mainThread.addMessage(connectMsg);
+					String msg = userDeets.getUserName() + " connected";
+					Message connectMsg = new Message(rootDeets, msg);
+					mainThread.addMessage(connectMsg);
 				}
-				
+
 			} while (actionRequest.getAction() != ActionTypes.CSDISCONNECT);
 			String msg = userDeets.getUserName() + " disconnected";
 			Message disConnectMsg = new Message(rootDeets, msg);
 			mainThread.addMessage(disConnectMsg);
 			System.out.println(msg);
-			scStream.close(); 
+			scStream.close();
 			outSocket.close();
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 
 }
