b3d2255eb57997fd8da9e13ea5485f12232916b2
Graham <ghildebrandt@outlook.com>
2015-05-29 12:50:14 -0400

    nah

--- Client Server Tests/src/client/ServerInComms.java
+++ Client Server Tests/src/client/ServerInComms.java
@@ -39,7 +39,7 @@ public class ServerInComms extends Thread{
 					ClientMain.setRemoteIndex(actionRequest.getIndex());
 					int difference = ClientMain.getRemoteIndex() - ClientMain.getLocalIndexLength();
 					if(difference != 0){
-						for (int i = 0; i <= difference; i++) {
+						for (int i = 0; i < difference; i++) {
 							System.out.println("Inside diff for loop, i = " + i);
 							System.out.println("Inside diff for loop, diff = " + difference);
 							ClientMain.localIndexAddIndex();
