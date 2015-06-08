839df2f66c639089b0e6163058cce52214242d0b
Graham <ghildebrandt@outlook.com>
2015-06-08 13:32:05 -0400

    added sending messages from server

--- Client Server Tests/src/server/ServerCommands.java
+++ Client Server Tests/src/server/ServerCommands.java
@@ -3,28 +3,47 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import sharedPackages.LoginDeets;
+import sharedPackages.Message;
 
 
 public class ServerCommands extends Thread {
 	private BufferedReader commandIn;
-	private String command = "";
+	private LoginDeets root;
 	
 	public ServerCommands(){
 		commandIn = new BufferedReader(new InputStreamReader(System.in));
+		root = new LoginDeets("root", "dank");
 	}
 	
 	public void run(){
-		while(!(command.equals("/close"))){
+		String input = "";
+		while(!(input.equals("close"))){
 			try {
-				command = commandIn.readLine();
+				input = commandIn.readLine();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+			String[] temp = input.split(" ");
+			String command = temp[0];
+			String[] args = null;
+			if(temp.length > 1){
+				args = Arrays.copyOfRange(temp, 1, temp.length);
+			}
+			if(command.equals("message")){
+				String messageContent = String.join(" ", args);
+				Message message = new Message(root, messageContent);
+				mainThread.addMessage(message);
+			}
 			
 		}
 		
 	}
+	
+	
+	
 
 }
