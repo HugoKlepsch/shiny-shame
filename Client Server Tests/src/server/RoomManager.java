dde4581aa77708730de896b4bc19eddcca03d8d5
Graham <ghildebrandt@outlook.com>
2015-08-05 14:54:32 -0400

    added some stuff to room manager

--- Client Server Tests/src/server/RoomManager.java
+++ Client Server Tests/src/server/RoomManager.java
@@ -8,6 +8,9 @@
 
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Vector;
 
 /**
@@ -15,7 +18,26 @@ import java.util.Vector;
  *
  */
 public class RoomManager {
+	public static int outPort = 6969; 
+	private static ServerSocket serverSocket;
 	
 	private static Vector<Room> rooms;
+	
+	public static void main(String[] args) throws IOException {
+		String selfIP = getSelfIP(); // this method returns the local IP address of the server
+		System.out.println("Self IP: " + selfIP); // we print out the local IP address
+
+		// TODO this is where we would load the database, if we implemented that.
+
+		serverSocket = new ServerSocket(outPort);
+		
+	}
+	
+	private static String getSelfIP() throws IOException {
+		Socket ipTest = new Socket("192.168.1.1", 80); 
+		String selfIP = ipTest.getLocalAddress().getHostAddress(); 
+		ipTest.close();
+		return selfIP; 
+	}
 
 }
